2d7d01b72de11bf2cd0e160fe1574a4c498d9617
xilu56 <xilu56@gmail.com>
2025-05-10 15:17:49 -0700

    Apply the paragraph tags

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -5,14 +5,23 @@ export default function Lab1() {
         <h3>HTML Examples</h3>
         <div id="wd-h-tag"> ... </div>
         <div id="wd-p-tag">
-          <h4>Paragraph Tag</h4>
-          <p id="wd-p-1"> ... </p>
-  This is the first paragraph. The paragraph tag is used to format vertical gaps between long pieces of text like this one.
-  
-  This is the second paragraph. Even though there is a deliberate white gap between the paragraph above and this paragraph, by default browsers render them as one contiguous piece of text as shown here on the right.
-  
-  This is the third paragraph. Wrap each paragraph with the paragraph tag to tell browsers to render the gaps.
-        </div>
+        <h4>Paragraph Tag</h4>
+        <p id="wd-p-1"> ... </p>
+        <p id="wd-p-2">
+This is the first paragraph. The paragraph tag is used to format
+vertical gaps between long pieces of text like this one.
+        </p>
+        <p id="wd-p-3">
+This is the second paragraph. Even though there is a deliberate white
+gap between the paragraph above and this paragraph, by default
+browsers render them as one contiguous piece of text as shown here on
+the right.
+        </p>
+        <p id="wd-p-4">
+This is the third paragraph. Wrap each paragraph with the paragraph
+tag to tell browsers to render the gaps.
+        </p>
+      </div>
       </div>
     );
   }
