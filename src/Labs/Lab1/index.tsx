0fc44d92ca6097e8de8d53392f7708cb6c14a475
xilu56 <xilu56@gmail.com>
2025-05-10 20:05:46 -0700

    Form: Textareas

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -17,6 +17,9 @@ export default function Lab1() {
                  title="The last name"
                  id="wd-text-fields-last-name" />
         </form>
+        <h5>Text boxes</h5>
+        <label>Biography:</label><br/>
+        <textarea id="wd-textarea" cols={30} rows={10}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
       </div>      
 );
 }
