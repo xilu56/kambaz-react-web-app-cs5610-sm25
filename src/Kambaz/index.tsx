84e0c3bdd653db9f8e494d5a76d70a373cb0e1e1
xilu56 <xilu56@gmail.com>
2025-05-10 23:17:12 -0700

    Kambaz - Navigation Sidebar - Link to NEU, Navigation Sidebar - Link to Account, Navigation Sidebar - Link to Dashboard

--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -1,15 +1,28 @@
 import { Routes, Route, Navigate } from "react-router";
 import Account from "./Account";
 import Dashboard from "./Dashboard";
+import KambazNavigation from "./Navigation";
+
 export default function Kambaz() {
   return (
     <div id="wd-kambaz">
       <h1>Kambaz</h1>
-      <Routes>
-        <Route path="/" element={<Navigate to="Account" />} />
-        <Route path="/Account/*" element={<Account />} />
-        <Route path="/Dashboard" element={<Dashboard />} />
-      </Routes>
+      <table>
+        <tbody>
+          <tr>
+            <td valign="top">
+              <KambazNavigation />
+            </td>
+            <td valign="top">
+              <Routes>
+                <Route path="/" element={<Navigate to="Account" />} />
+                <Route path="/Account/*" element={<Account />} />
+                <Route path="/Dashboard" element={<Dashboard />} />
+              </Routes>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
