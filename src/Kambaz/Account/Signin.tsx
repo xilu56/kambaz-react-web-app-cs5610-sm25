fe87275c362d8860cb975c1ae5730532d2dbc807
xilu56 <xilu56@gmail.com>
2025-05-10 21:27:46 -0700

    Some default values

--- src/Kambaz/Account/Signin.tsx
+++ src/Kambaz/Account/Signin.tsx
@@ -1,13 +1,29 @@
 import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 export default function Signin() {
   const navigate = useNavigate();
+  const [username, setUsername] = useState("kambaz");
+  const [password, setPassword] = useState("123456");
+
   return (
     <div id="wd-signin-screen">
       <h3>Sign in</h3>
-      <input placeholder="username" className="wd-username" /> <br />
-      <input placeholder="password" type="password" className="wd-password" /> <br />
+      <input
+        placeholder="username"
+        className="wd-username"
+        value={username}
+        onChange={e => setUsername(e.target.value)}
+      /> <br />
+      <input
+        placeholder="password"
+        type="password"
+        className="wd-password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      /> <br />
       <button id="wd-signin-btn" onClick={() => navigate("/Kambaz/Account/Profile")}>Sign in</button> <br />
-      <Link  to="/Kambaz/Account/Signup"  id="wd-signup-link">Sign up</Link>
+      <Link to="/Kambaz/Account/Signup" id="wd-signup-link">Sign up</Link>
     </div>
   );
 }
