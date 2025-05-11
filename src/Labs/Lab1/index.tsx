ffbdb97780a01d42e3bd638bbbbfc09943e3eedb
xilu56 <xilu56@gmail.com>
2025-05-10 20:21:11 -0700

    Select many options

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -38,6 +38,14 @@ export default function Lab1() {
             Science Fiction</option>
         <option value="FANTASY">Fantasy</option>
         </select>
+        <h5>Select many</h5>
+        <label  htmlFor="wd-select-many-genre"> Favorite movie genres: </label><br/>
+        <select multiple id="wd-select-many-genre">
+        <option value="COMEDY" selected> Comedy          </option>
+        <option value="DRAMA">           Drama           </option>
+        <option value="SCIFI"  selected> Science Fiction </option>
+        <option value="FANTASY">         Fantasy         </option>
+        </select>
       </div>      
 );
 }
