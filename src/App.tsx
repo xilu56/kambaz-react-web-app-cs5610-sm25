654a7775a18907709b52b6ab6dd7c728d0e68a55
xilu56 <xilu56@gmail.com>
2025-05-10 20:44:56 -0700

    Implementing Navigation in Single Page Applications

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import Labs from "./Labs";
+import Kambaz from "./Kambaz";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 export default function App() {
   return (
-    <div>
-      <Labs />
-    </div>
-);}
+    <HashRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="Labs" />} />
+          <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kambaz/*" element={<Kambaz />} />
+        </Routes>
+      </div>
+    </HashRouter> );}
--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -1,36 +1,6 @@
 export default function Lab1() {
     return (
-        <div id="wd-forms">
-        <h4>Other HTML field types</h4>
-
-<label htmlFor="wd-text-fields-email"> Email: </label>
-<input type="email"
-       placeholder="jdoe@somewhere.com"
-       id="wd-text-fields-email"/><br/>
-
-<label htmlFor="wd-text-fields-salary-start"> Starting salary:</label>
-<input type="number"
-       value="100000"
-       placeholder="1000"
-       id="wd-text-fields-salary-start"/><br/>
-
-<label htmlFor="wd-text-fields-rating"> Rating: </label>
-<input type="range"
-       value="4"
-       max="5"
-       placeholder="Doe"
-       id="wd-text-fields-rating"/><br/>
-
-<label htmlFor="wd-text-fields-dob"> Date of birth: </label>
-<input type="date"
-       value="2000-01-21"
-       id="wd-text-fields-dob"/><br/>
-
-<h4>Anchor tag</h4>
-Please
-<a href="https://www.lipsum.com" id="wd-lipsum">click here</a>
-to get dummy text<br/>
-      </div>      
-);
-}
-  
+      <div>
+        <h2>Lab 1</h2>
+      </div>
+  );}
