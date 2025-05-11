dcdfc88c70d6422c60a26671310ffa6278cceb88
xilu56 <xilu56@gmail.com>
2025-05-10 20:07:59 -0700

    Clicking button pops up alert

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -20,6 +20,13 @@ export default function Lab1() {
         <h5>Text boxes</h5>
         <label>Biography:</label><br/>
         <textarea id="wd-textarea" cols={30} rows={10}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
+        <h5 id="wd-buttons">Buttons</h5>
+        <button type="button"
+                onClick={() => alert("Life is Good!")}
+                id="wd-all-good">
+        Hello World!
+        </button>
+
       </div>      
 );
 }
