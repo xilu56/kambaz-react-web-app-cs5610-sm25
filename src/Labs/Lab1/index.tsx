a8460e82e47ede5789a718e32feb1886f13d5634
xilu56 <xilu56@gmail.com>
2025-05-10 20:04:17 -0700

    Forms: Username, Password, First Name, Last Name

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -1,19 +1,23 @@
 export default function Lab1() {
     return (
-<div id="wd-images">
-  <h4>Image tag</h4>
-  Loading an image from the internet: <br />
-  <img id="wd-starship" width="400px"   src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
-  <br />
-  Loading a local image:
-  <br />
-  <img src="src/assets/starship.jpg"
-     width="200px"
-     height="300px"
-/>
-<br />
-    </div>
-  );
+        <div id="wd-forms">
+        <h4>Form Elements</h4>
+        <form id="wd-text-fields">
+          <h5>Text Fields</h5>
+          <label htmlFor="wd-text-fields-username">Username:</label>
+          <input placeholder="jdoe" id="wd-text-fields-username" /> <br />
+          <label htmlFor="wd-text-fields-password">Password:</label>
+          <input type="password" value="123@#$asd" id="wd-text-fields-password" />
+          <br />
+          <label htmlFor="wd-text-fields-first-name">First name:</label>
+          <input type="text" title="John" id="wd-text-fields-first-name" /> <br />
+          <label htmlFor="wd-text-fields-last-name">Last name:</label>
+          <input type="text" placeholder="Doe"
+                 value="Wonderland"
+                 title="The last name"
+                 id="wd-text-fields-last-name" />
+        </form>
+      </div>      
+);
 }
-
-// Compare this snippet from src/Labs/Lab1/index.tsx:
+  
