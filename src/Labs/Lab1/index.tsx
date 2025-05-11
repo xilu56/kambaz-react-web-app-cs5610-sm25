fe5161726c7a49fbdd686e36b8c25c547ff4f625
xilu56 <xilu56@gmail.com>
2025-05-10 19:00:50 -0700

    Teslabot image

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -1,9 +1,19 @@
 export default function Lab1() {
     return (
-<img src="src/assets/starship.jpg"
+<div id="wd-images">
+  <h4>Image tag</h4>
+  Loading an image from the internet: <br />
+  <img id="wd-starship" width="400px"   src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
+  <br />
+  Loading a local image:
+  <br />
+  <img src="src/assets/starship.jpg"
      width="200px"
      height="300px"
 />
-    );
+<br />
+    </div>
+  );
 }
+
 // Compare this snippet from src/Labs/Lab1/index.tsx:
