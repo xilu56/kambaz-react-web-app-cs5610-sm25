efcac073297b2b79b99f8c5cac54a1c10fea6ad1
xilu56 <xilu56@gmail.com>
2025-05-10 20:18:07 -0700

    Comedy, drama, Scifi, Fantasy checkbox

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -30,20 +30,20 @@ export default function Lab1() {
         <h5 id="wd-file-upload">File Upload</h5>
         <label htmlFor="wd-file-upload-input">Upload a file:</label>
         <input type="file" id="wd-file-upload-input" />
-        <h5 id="wd-radio-buttons">Radio buttons</h5>
+        <h5 id="wd-checkboxes">Checkboxes</h5>
+        <label>Favorite movie genre:</label><br/>
 
-        <label>Favorite movie genre:</label><br />
+        <input type="checkbox" name="check-genre" id="wd-chkbox-comedy"/>
+        <label htmlFor="wd-chkbox-comedy">Comedy</label><br/>
 
-        <input type="radio" name="radio-genre" id="wd-radio-comedy"/>
-        <label htmlFor="wd-radio-comedy">Comedy</label><br />
+        <input type="checkbox" name="check-genre" id="wd-chkbox-drama"/>
+        <label htmlFor="wd-chkbox-drama">Drama</label><br/>
 
-        <input type="radio" name="radio-genre" id="wd-radio-drama"/>
-        <label htmlFor="wd-radio-drama">Drama</label><br />
+        <input type="checkbox" name="check-genre" id="wd-chkbox-scifi"/>
+        <label htmlFor="wd-chkbox-scifi">Science Fiction</label><br/>
 
-        <input type="radio" name="radio-genre" id="wd-radio-scifi"/>
-        <label htmlFor="wd-radio-scifi">SciFi</label><br />
-        <input type="radio" name="radio-genre" id="wd-radio-fantasy"/>
-        <label htmlFor="wd-radio-fantasy">Fantasy</label>
+        <input type="checkbox" name="check-genre" id="wd-chkbox-fantasy"/>
+        <label htmlFor="wd-chkbox-fantasy">Fantasy</label>
       </div>      
 );
 }
