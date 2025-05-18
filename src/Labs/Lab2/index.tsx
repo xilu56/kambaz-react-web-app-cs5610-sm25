c4de0c8bf6978fe9855ba3421f1bda78df12da2d
xilu56 <xilu56@gmail.com>
2025-05-18 15:13:44 -0700

    ID Selectors - White on red paragraph; ID Selectors - Black on yellow paragraph

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,13 +1,20 @@
+import "./index.css";
 export default function Lab2() {
   return (
     <div id="wd-lab2">
       <h2>Lab 2 - Cascading Style Sheets</h2>
       <h3>Styling with the STYLE attribute</h3>
-      <p style={{ backgroundColor: "red",
-                  color: "white" }}>
-        Style attribute allows configuring look and feel
-        right on the element. Although it's very convenient
-        it is considered bad practice and you should avoid
-        using the style attribute
-      </p>
-    </div>);}
+      …
+      <div id="wd-css-id-selectors">
+        <h3>ID selectors</h3>
+        <p id="wd-id-selector-1">
+Instead of changing the look and feel of all the 
+elements of the same name, e.g., P, we can refer to a specific element by its ID
+        </p>
+        <p id="wd-id-selector-2">
+Here's another paragraph using a different ID and a different look and
+          feel
+        </p>
+      </div>
+    </div>
+);}
