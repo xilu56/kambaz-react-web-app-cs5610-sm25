29dd583c41e2fd18bd881539139efdcdbc4f4afd
xilu56 <xilu56@gmail.com>
2025-05-18 21:32:45 -0700

    Borders - Fat red border; Thin blue dashed border

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,6 +1,7 @@
 import "./index.css";
 import ForegroundColor from "./ForegroundColors";
 import BackgroundColor from "./BackgroundColors";
+import Borders from "./Borders";
 
 export default function Lab2() {
   return (
@@ -8,6 +9,7 @@ export default function Lab2() {
       <h1>Lab 2</h1>
       <ForegroundColor />
       <BackgroundColor />
+      <Borders />
     </div>
 
   );}
