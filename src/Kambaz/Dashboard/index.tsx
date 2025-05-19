b7045119d45c250b5d5ca868a8f7e6fbd4996696
xilu56 <xilu56@gmail.com>
2025-05-19 15:32:07 -0700

    Courses render as a grid

--- src/Kambaz/Dashboard/index.tsx
+++ src/Kambaz/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { Row, Col, Card, Button } from "react-bootstrap";
 
 export default function Dashboard() {
   // Course data
@@ -50,25 +51,31 @@ export default function Dashboard() {
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-      <h2 id="wd-dashboard-published">Published Courses (7)</h2> <hr />
+      <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
       <div id="wd-dashboard-courses">
-        {courses.map((course) => (
-          <div key={course.id} className="wd-dashboard-course">
-            <Link
-              to={`/Kambaz/Courses/${course.id}/Home`}
-              className="wd-dashboard-course-link"
-            >
-              <img src={course.image} width={200} alt={course.title} />
-              <div>
-                <h5>{course.title}</h5>
-                <p className="wd-dashboard-course-title">
-                  {course.description}
-                </p>
-                <button>Go</button>
-              </div>
-            </Link>
-          </div>
-        ))}
+        <Row xs={1} sm={2} md={3} lg={4} xl={5} className="g-4">
+          {courses.map((course) => (
+            <Col key={course.id} className="wd-dashboard-course" style={{ width: "300px" }}>
+              <Card>
+                <Link
+                  to={`/Kambaz/Courses/${course.id}/Home`}
+                  className="wd-dashboard-course-link text-decoration-none text-dark"
+                >
+                  <Card.Img variant="top" src={course.image} width="100%" height={160} alt={course.title} />
+                  <Card.Body>
+                    <Card.Title className="wd-dashboard-course-title text-nowrap overflow-hidden">
+                      {course.title}
+                    </Card.Title>
+                    <Card.Text className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
+                      {course.description}
+                    </Card.Text>
+                    <Button variant="primary">Go</Button>
+                  </Card.Body>
+                </Link>
+              </Card>
+            </Col>
+          ))}
+        </Row>
       </div>
     </div>
   );
