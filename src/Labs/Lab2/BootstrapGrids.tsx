71021b8262560aefec76d902a93bb7a003924e33
xilu56 <xilu56@gmail.com>
2025-05-18 23:30:36 -0700

    Columns 1, 2, through 12 laid out horizontally or vertically based on screen size

--- src/Labs/Lab2/BootstrapGrids.tsx
+++ src/Labs/Lab2/BootstrapGrids.tsx
@@ -34,7 +34,7 @@ export default function BootstrapGrids() {
                     <h3>Sidebar</h3>
                 </Col>
             </Row>
-        </div>
+
         <div id="wd-bs-responsive-grids">
   <h2>Responsive grid system</h2>
   <Row>
@@ -57,7 +57,49 @@ export default function BootstrapGrids() {
   </Row>
 </div>
 
+<div id="wd-bs-responsive-dramatic">
+  <h2>Responsive grid system</h2>
+  <Row>
+    <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={1} 
+         className="bg-warning">
+         <h4>1</h4></Col>
+    <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={1}
+         className="bg-primary text-white">
+         <h4>2</h4></Col>
+    <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={1}
+         className="bg-danger text-white">
+         <h4>3</h4></Col>
+    <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={1}
+         className="bg-success text-white">
+         <h4>4</h4></Col>
+    <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={1}
+         className="bg-warning">
+         <h4>5</h4></Col>
+    <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={1}
+         className="bg-primary text-white">
+         <h4>6</h4></Col>
+    <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={1}
+         className="bg-danger text-white">
+         <h4>7</h4></Col>
+    <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={1}
+         className="bg-success text-white">
+         <h4>8</h4></Col>
+    <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={1}
+         className="bg-warning">
+<h4>9</h4></Col>
+    <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={1}
+         className="bg-primary text-white">
+         <h4>10</h4></Col>
+    <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={1}
+         className="bg-danger text-white">
+         <h4>11</h4></Col>
+   <Col  xs={12} sm={6} md={4} lg={3} xl={2} xxl={1}
+         className="bg-success text-white">
+         <h4>12</h4></Col>
+   </Row>
+</div>
+        </div>
     </div>
-    
-    );
+    )
 }
+// Compare this snippet from src/Labs/Lab2/BootstrapGrids.tsx:
