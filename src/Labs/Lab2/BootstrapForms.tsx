7126c3744e191f0a99cee4757c0d538b958786a8
xilu56 <xilu56@gmail.com>
2025-05-19 00:21:50 -0700

    Switches styled as shown

--- src/Labs/Lab2/BootstrapForms.tsx
+++ src/Labs/Lab2/BootstrapForms.tsx
@@ -2,6 +2,8 @@ import FormGroup from 'react-bootstrap/FormGroup';
 import FormLabel from 'react-bootstrap/FormLabel';
 import FormControl from 'react-bootstrap/FormControl';
 import FormSelect from 'react-bootstrap/FormSelect';
+import Form from 'react-bootstrap/Form';
+
 
 export default function BootstrapForms() {
     return (
@@ -22,6 +24,20 @@ export default function BootstrapForms() {
      <option value="2">Two</option>
      <option value="3">Three</option>
   </FormSelect>
+  <div id="wd-css-styling-switches">
+  <h3>Switches</h3>
+  <Form.Check type="switch" checked={false} id="wd-switch-1"
+              label="Unchecked switch checkbox input"/>
+  <Form.Check type="switch" checked={true}  id="wd-switch-2"
+              label="Checked switch checkbox input"/>
+  <Form.Check type="switch" checked={false} disabled
+              id="custom-switch"
+              label="Unchecked disabled switch checkbox input"/>
+  <Form.Check type="switch" checked={true} disabled 
+              id="custom-switch"
+              label="Checked disabled switch checkbox input"/>
+</div>
+
 </div>
 
     )
