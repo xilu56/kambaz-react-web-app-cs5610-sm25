482849ebab04c5d1b49f9b01292deb0dc7e9381f
xilu56 <xilu56@gmail.com>
2025-05-18 18:27:47 -0700

    Background Color - White on blue heading; Black on red paragraph; White on green span

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -2,12 +2,15 @@ import "./index.css";
 export default function Lab2() {
   return (
   
-    <div id="wd-css-colors">
-    <h2>Colors</h2>
-    <h3 className="wd-fg-color-blue">Foreground color</h3>
-    <p className="wd-fg-color-red">
-      The text in this paragraph is red but
-      <span className="wd-fg-color-green"> this text is green</span>
-    </p>
-    </div>
+  <div id="wd-css-background-colors">
+  <h3 className="wd-bg-color-blue wd-fg-color-white">Background color</h3>
+  <p className="wd-bg-color-red wd-fg-color-black">
+    This background of this paragraph is red but 
+    <span className="wd-bg-color-green wd-fg-color-white">
+      the background of this text is green and the foreground white
+      </span>
+  </p>
+</div>
+
+
   );}
