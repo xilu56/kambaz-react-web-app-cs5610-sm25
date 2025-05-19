5852732aa9c60a63f4702e85e07abd88d2de4674
xilu56 <xilu56@gmail.com>
2025-05-19 00:03:59 -0700

    Link Lists - Favorite list of links to books styled as shown

--- src/Labs/Lab2/BootstrapLists.tsx
+++ src/Labs/Lab2/BootstrapLists.tsx
@@ -11,6 +11,31 @@ export default function BootstrapLists() {
     <ListGroup.Item>Lord of the Ring</ListGroup.Item>
     <ListGroup.Item disabled>Star Wars</ListGroup.Item>
   </ListGroup>
+  
+  <h3>Favorite books</h3>
+  <ListGroup>
+    <ListGroup.Item   action active
+      href="https://en.wikipedia.org/wiki/Dune_(novel)">
+      Dune                 </ListGroup.Item>
+    <ListGroup.Item   action
+      href="https://en.wikipedia.org/wiki/The_Lord_of_the_Rings">
+      Lord of the Rings    </ListGroup.Item>
+    <ListGroup.Item   action
+      href="https://en.wikipedia.org/wiki/The_Forever_War">
+      The Forever War      </ListGroup.Item>
+    <ListGroup.Item   action
+href="https://en.wikipedia.org/wiki/2001:_A_Space_Odyssey_(novel)">
+      2001 A Space Odyssey </ListGroup.Item>
+    <ListGroup.Item   action   disabled
+      href="https://en.wikipedia.org/wiki/Ender%27s_Game">
+      Ender's Game         </ListGroup.Item>
+    <ListGroup.Item action onClick={() => alert("New book added")}>
+      Add another book     </ListGroup.Item>
+  </ListGroup>
+
 </div>
+
+
+
   )
 }   
