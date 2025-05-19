2f11d254ecba3ad3f5636522ef10157ab2229785
xilu56 <xilu56@gmail.com>
2025-05-19 15:03:24 -0700

    Kambaz - Navigation - Links: Northeaster, Account, Dashboard, Courses, Calendar, Inbox, Labs

--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -1,33 +1,26 @@
-import { Routes, Route, Navigate } from "react-router";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Row, Col } from "react-bootstrap";
+import KambazNavigation from "./Navigation";
 import Account from "./Account";
 import Dashboard from "./Dashboard";
-import KambazNavigation from "./Navigation";
 import Courses from "./Courses";
 
 export default function Kambaz() {
   return (
     <div id="wd-kambaz">
-      <h1>Kambaz</h1>
-      <table>
-        <tbody>
-          <tr>
-            <td valign="top">
-              <KambazNavigation />
-            </td>
-            <td valign="top">
-              <Routes>
-                <Route path="/" element={<Navigate to="Account" />} />
-                <Route path="/Account/*" element={<Account />} />
-                <Route path="/Dashboard" element={<Dashboard />} />
-                <Route path="/Courses" element={<Courses />} />
-                <Route path="/Courses/:cid/*" element={<Courses />} />
-                <Route path="/Calendar" element={<h1>Calendar</h1>} />
-                <Route path="/Inbox" element={<h1>Inbox</h1>} />
-                </Routes>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <Row>
+        <Col md={2} className="bg-light min-vh-100">
+          <KambazNavigation />
+        </Col>
+        <Col md={10} className="p-4">
+          <Routes>
+            <Route path="/" element={<Navigate to="Account" />} />
+            <Route path="/Account/*" element={<Account />} />
+            <Route path="/Dashboard" element={<Dashboard />} />
+            <Route path="/Courses/:cid/*" element={<Courses />} />
+          </Routes>
+        </Col>
+      </Row>
     </div>
   );
 }
