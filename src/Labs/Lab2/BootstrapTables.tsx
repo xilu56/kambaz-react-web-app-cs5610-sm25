d8d6152c829299869ef14394ea8cc06494cfb8ec
xilu56 <xilu56@gmail.com>
2025-05-18 23:53:32 -0700

    Responsive Tables - Table with long columns shows horizontal scrollbars when narrow window

--- src/Labs/Lab2/BootstrapTables.tsx
+++ src/Labs/Lab2/BootstrapTables.tsx
@@ -17,7 +17,25 @@ export default function BootstrapTables() {
       <tr className="table-success"><td colSpan={3}>Average</td><td>90</td></tr>
     </tfoot>
   </Table>
-
+  
+  <div id="wd-css-responsive-tables">
+  <h2>Responsive tables</h2>
+  <Table responsive>
+    <thead>
+      <tr><th>Very</th><th>long</th><th>set</th><th>of</th><th>columns</th>
+          <th>Very</th><th>long</th><th>set</th><th>of</th><th>columns</th>
+          <th>Very</th><th>long</th><th>set</th><th>of</th><th>columns</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+         <td>Very</td><td>long</td><td>set</td><td>of</td><td>columns</td>
+         <td>Very</td><td>long</td><td>set</td><td>of</td><td>columns</td>
+         <td>Very</td><td>long</td><td>set</td><td>of</td><td>columns</td>
+      </tr>
+    </tbody>
+  </Table>
+</div>
     </div>
   );
 }
