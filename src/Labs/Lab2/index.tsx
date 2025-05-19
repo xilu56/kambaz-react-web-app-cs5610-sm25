263e63e185c3ac6d4e25f48d8e2df058374435b4
xilu56 <xilu56@gmail.com>
2025-05-18 18:47:41 -0700

    Made a seperate file for Background Color

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,11 +1,13 @@
 import "./index.css";
 import ForegroundColor from "./ForegroundColors";
+import BackgroundColor from "./BackgroundColors";
 
 export default function Lab2() {
   return (
     <div id="wd-lab2">
       <h1>Lab 2</h1>
       <ForegroundColor />
+      <BackgroundColor />
     </div>
 
   );}
