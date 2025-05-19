7404eacc3d091d011ff629895a1d7a2e7930e980
xilu56 <xilu56@gmail.com>
2025-05-19 00:28:38 -0700

    Vertical in narrow screens

--- src/Labs/Lab2/BootstrapForms.tsx
+++ src/Labs/Lab2/BootstrapForms.tsx
@@ -7,6 +7,7 @@ import FormRange from 'react-bootstrap/FormRange';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 
 export default function BootstrapForms() {
@@ -88,6 +89,48 @@ export default function BootstrapForms() {
     </Col>
   </Form.Group>
 </div>
+<div id="wd-css-responsive-forms-2">
+  <h3>Responsive forms</h3>
+  <Form>
+   <Form.Group as={Row} className="mb-3">
+     <Form.Label column sm={2}> Email </Form.Label>
+     <Col sm={10}>
+       <Form.Control type="email" placeholder="Email" />
+     </Col>
+   </Form.Group>
+   <Form.Group as={Row} className="mb-3">
+     <Form.Label column sm={2}> Password </Form.Label>
+     <Col sm={10}>
+       <Form.Control type="password" placeholder="Password" />
+     </Col>
+   </Form.Group>
+   <fieldset>
+     <Form.Group as={Row} className="mb-3">
+       <Form.Label as="legend" column sm={2}>
+         Radios </Form.Label>
+       <Col sm={10}>
+         <Form.Check type="radio" label="first radio"
+           checked name="formHorizontalRadios"/>
+         <Form.Check type="radio" label="second radio"
+           name="formHorizontalRadios"/>
+         <Form.Check type="radio" label="third radio"
+           name="formHorizontalRadios"/>
+       </Col>
+     </Form.Group>
+   </fieldset>
+   <Form.Group as={Row} className="mb-3">
+     <Col sm={{ span: 10, offset: 2 }}>
+       <Form.Check label="Remember me" />
+     </Col>
+   </Form.Group>
+   <Form.Group as={Row} className="mb-3">
+     <Col>
+       <Button type="submit">Sign in</Button>
+     </Col>
+   </Form.Group>
+  </Form>
+</div>
+
 </div>
     )
 }
