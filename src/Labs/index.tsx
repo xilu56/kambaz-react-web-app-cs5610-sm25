931a55323bff9a565d129528befbbda5b2174fc2
xilu56 <xilu56@gmail.com>
2025-05-18 20:13:44 -0700

    Updating lab 1 page for regrading

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,18 +1,26 @@
 import Lab1 from "./Lab1";
-import { Route, Routes, Navigate } from "react-router";
-import TOC from "./TOC";
+import { Route, Routes } from "react-router";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
+
 export default function Labs() {
   return (
     <div>
       <h1>Labs</h1>
-        <TOC />
       <Routes>
-        <Route path="/" element={<Navigate to="Lab1" />} />
+        <Route path="/" element={<div>
+          <h2>Laboratory Assignments</h2>
+          <p>Please select a specific lab from the navigation to view its content:</p>
+          <ul>
+            <li>Lab 1: HTML Basics</li>
+            <li>Lab 2: CSS Styling</li>
+            <li>Lab 3: JavaScript Fundamentals</li>
+          </ul>
+        </div>} />
         <Route path="Lab1" element={<Lab1 />} />
         <Route path="Lab2" element={<Lab2 />} />
         <Route path="Lab3" element={<Lab3 />} />
       </Routes>
     </div>
-);}
+  );
+}
