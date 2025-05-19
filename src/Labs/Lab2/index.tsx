bb426252fff3b382c509c2dc3814326c60dceb53
xilu56 <xilu56@gmail.com>
2025-05-18 18:21:34 -0700

    Foreground Color - Blue on white heading; Foreground Color - Red on white text; Foreground Color - Green on white text

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,25 +1,13 @@
 import "./index.css";
 export default function Lab2() {
   return (
-    <div id="wd-css-document-structure">
-  <div className="wd-selector-1">
-    <h3>Document structure selectors</h3>
-    <div className="wd-selector-2">
-      Selectors can be combined to refer elements in particular
-      places in the document
-      <p className="wd-selector-3">
-        This paragraph's red background is referenced as
-        <br />
-        .selector-2 .selector3<br />
-        meaning the descendant of some ancestor.<br />
-        <span className="wd-selector-4">
-          Whereas this span is a direct child of its parent
-        </span><br />
-          You can combine these relationships to create specific 
-          styles depending on the document structure
-      </p>
+  
+    <div id="wd-css-colors">
+    <h2>Colors</h2>
+    <h3 className="wd-fg-color-blue">Foreground color</h3>
+    <p className="wd-fg-color-red">
+      The text in this paragraph is red but
+      <span className="wd-fg-color-green"> this text is green</span>
+    </p>
     </div>
-  </div>
-</div>
-
-);}
+  );}
