af3a1142fdbf8ce1179ec0d9a8fdceff92814734
xilu56 <xilu56@gmail.com>
2025-05-18 23:24:53 -0700

    Responsive - Columns A, B, C, D laid out horizontally or vertically based on screen size

--- src/Labs/Lab2/BootstrapGrids.tsx
+++ src/Labs/Lab2/BootstrapGrids.tsx
@@ -35,6 +35,29 @@ export default function BootstrapGrids() {
                 </Col>
             </Row>
         </div>
+        <div id="wd-bs-responsive-grids">
+  <h2>Responsive grid system</h2>
+  <Row>
+    <Col xs={12} md={6} xl={3}
+         className="bg-warning">
+         <h3>Column A</h3>
+    </Col>
+    <Col xs={12} md={6} xl={3}
+         className="bg-primary text-white">
+         <h3>Column B</h3>
+    </Col>
+    <Col xs={12} md={6} xl={3}
+         className="bg-danger text-white">
+         <h3>Column C</h3>
+    </Col>
+    <Col xs={12} md={6} xl={3}
+         className="bg-success text-white">
+         <h3>Column D</h3>
+    </Col>
+  </Row>
+</div>
+
     </div>
+    
     );
 }
