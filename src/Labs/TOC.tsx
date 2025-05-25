e452bf7fed8f8edac836c8dd56caa7a8b903d9a0
xilu56 <xilu56@gmail.com>
2025-05-25 15:33:29 -0700

    Working with Location

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,67 +1,18 @@
 import Nav from "react-bootstrap/Nav";
-import { NavLink, useLocation } from "react-router-dom";
-
+import { useLocation } from "react-router";
+import { Link } from "react-router-dom";
 export default function TOC() {
-  const location = useLocation();
-  const { pathname } = location;
-  
+  const { pathname } = useLocation();
   return (
-   <Nav variant="pills">
-     <Nav.Item>
-       <Nav.Link 
-         as={NavLink} 
-         to="/"
-         className={pathname === "/" ? "active" : ""}
-       >
-         Labs
-       </Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-       <Nav.Link 
-         as={NavLink} 
-         to="/Labs/Lab1"
-         className={pathname === "/Labs/Lab1" ? "active" : ""}
-       >
-         Lab 1
-       </Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-       <Nav.Link 
-         as={NavLink} 
-         to="/Labs/Lab2"
-         className={pathname === "/Labs/Lab2" ? "active" : ""}
-       >
-         Lab 2
-       </Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-       <Nav.Link 
-         as={NavLink} 
-         to="/Labs/Lab3"
-         className={pathname === "/Labs/Lab3" ? "active" : ""}
-       >
-         Lab 3
-       </Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-       <Nav.Link 
-         as={NavLink} 
-         to="/Kambaz"
-         className={pathname.includes("/Kambaz") ? "active" : ""}
-       >
-         Kambaz
-       </Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-       <Nav.Link 
-         href="https://github.com/xilu56/kambaz-react-web-app-cs5610-sm25.git"
-         target="_blank"
-         rel="noopener noreferrer"
-       >
-         My GitHub
-       </Nav.Link>
-     </Nav.Item>
-   </Nav>
+    <Nav variant="pills" id="wd-toc">
+      <Nav.Item> <Nav.Link as={Link} to="/Labs/Lab1" id="wd-a1"
+          active={pathname.includes("Lab1")}> Lab 1 </Nav.Link> </Nav.Item>
+      <Nav.Item> <Nav.Link as={Link} to="/Labs/Lab2" id="wd-a2"
+          active={pathname.includes("Lab2")}> Lab 2 </Nav.Link> </Nav.Item>
+      <Nav.Item> <Nav.Link as={Link} to="/Labs/Lab3" id="wd-a3"
+          active={pathname.includes("Lab3")}> Lab 3 </Nav.Link> </Nav.Item>
+      <Nav.Item> <Nav.Link as={Link} to="/Kambaz" id="wd-a3"> Kambaz </Nav.Link> </Nav.Item>
+      <Nav.Item> <Nav.Link href="https://github.com/xilu56/kambaz-react-web-app-cs5610-sm25.git" target="_blank"> My GitHub </Nav.Link> </Nav.Item>
+    </Nav>
   );
 }
-
