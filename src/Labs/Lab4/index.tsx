282bc556d9bd9c01d928418495fb47e99978c88b
xilu56 <xilu56@gmail.com>
2025-06-02 13:05:04 -0700

    Lab - User Events - Handling Click Events

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,10 +1,9 @@
+import ClickEvent from "./ClickEvent";
+
 export default function Lab4() {
   return (
-    <div id="wd-lab4">
-      <h3>React</h3>
-      <p>In this lab, we will explore the basics of React, including components, props, and state management.</p>
-      <p>We will also cover event handling and lifecycle methods.</p>
-      <p>Make sure to check out the individual components for more details.</p>
-    </div>
-  );
+    <div>
+      <h3>JavaScript Events</h3>
+      <ClickEvent />
+    </div>  );
 }
