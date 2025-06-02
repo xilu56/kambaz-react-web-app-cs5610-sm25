6777fb51ec2ad302b8858d4191a1510295cbc99e
xilu56 <xilu56@gmail.com>
2025-06-02 14:01:25 -0700

    Lab - Application State - Counter Redux - Dispatching Events to Reducers

--- src/Labs/Lab4/ReduxExamples/index.tsx
+++ src/Labs/Lab4/ReduxExamples/index.tsx
@@ -1,4 +1,6 @@
 import { useSelector} from "react-redux";
+import CounterRedux from "./CounterRedux";
+
 export default function HelloRedux() {
   const { message } = useSelector((state: any) => state.helloReducer);
   return (
@@ -6,6 +8,7 @@ export default function HelloRedux() {
         <h2>Redux Example</h2>
       <h3>Hello Redux</h3>
       <h4>{message}</h4> <hr />
+        <CounterRedux />
     </div>
   );
 }
