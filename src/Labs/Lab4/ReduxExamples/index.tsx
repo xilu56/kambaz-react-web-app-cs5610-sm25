c72784873bd58f3689f2e569c9bcf8689b029966
xilu56 <xilu56@gmail.com>
2025-06-02 15:05:20 -0700

    Lab - Todo List - Render all todos;Create new Todo; Delete Todo

--- src/Labs/Lab4/ReduxExamples/index.tsx
+++ src/Labs/Lab4/ReduxExamples/index.tsx
@@ -1,6 +1,7 @@
 import { useSelector} from "react-redux";
 import CounterRedux from "./CounterRedux";
 import AddRedux from "./AddRedux";
+import TodoList from "./todos/TodoList";
 
 export default function HelloRedux() {
   const { message } = useSelector((state: any) => state.helloReducer);
@@ -11,6 +12,7 @@ export default function HelloRedux() {
       <h4>{message}</h4> <hr />
         <CounterRedux />
         <AddRedux />
+        <TodoList />
     </div>
   );
 }
