e72ce378374643fe04368ecf7034a2e1c700b063
xilu56 <xilu56@gmail.com>
2025-06-02 13:45:22 -0700

    Lab - Component State - Array State Variables - addElement; deleteElement

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -7,6 +7,7 @@ import BooleanStateVariables from "./BooleanStateVariables";
 import StringStateVariables from "./StringStateVariables";
 import DateStateVariable from "./DateStateVariable";
 import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable"; 
 
 export default function Lab4() {
     function sayHello() {
@@ -24,5 +25,6 @@ export default function Lab4() {
         <StringStateVariables />
         <DateStateVariable />
         <ObjectStateVariable />
+        <ArrayStateVariable />
     </div>  );
     }
