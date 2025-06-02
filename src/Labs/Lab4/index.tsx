dec3d1d030385b0d4d38a1eb7d9b07968d5b3480
xilu56 <xilu56@gmail.com>
2025-06-02 13:13:01 -0700

    Lab - User Events - The Event Object

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,6 +1,7 @@
 import ClickEvent from "./ClickEvent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
 
 export default function Lab4() {
     function sayHello() {
@@ -12,6 +13,6 @@ export default function Lab4() {
         <ClickEvent />
         <PassingDataOnEvent />
         <PassingFunctions theFunction= {sayHello} />
+        <EventObject />
     </div>  );
     }
-    
