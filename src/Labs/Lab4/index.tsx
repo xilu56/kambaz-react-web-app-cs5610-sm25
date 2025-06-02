62d23ba925cf30e8077851a68bdff0dc3bb8b7a0
xilu56 <xilu56@gmail.com>
2025-06-02 13:10:40 -0700

    Lab - User Events - Passing Functions as Attributes

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -1,11 +1,17 @@
 import ClickEvent from "./ClickEvent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
 
 export default function Lab4() {
+    function sayHello() {
+        alert("Hello");
+    }
   return (
     <div>
       <h3>JavaScript Events</h3>
         <ClickEvent />
         <PassingDataOnEvent />
+        <PassingFunctions theFunction= {sayHello} />
     </div>  );
-}
+    }
+    
