0c204d03270048fb8d0cb766298488ac3fb2a79c
xilu56 <xilu56@gmail.com>
2025-06-09 19:52:52 -0700

    Update Todo To NodeJS Assignment

--- src/Labs/Lab5/WorkingWithArrays.js
+++ src/Labs/Lab5/WorkingWithArrays.js
@@ -64,5 +64,12 @@ export default function WorkingWithArrays(app) {
     console.log("Todos reset to original state");
     res.json(todos);
   });
+  app.get("/lab5/todos/:id/title/:title", (req, res) => {
+    const { id, title } = req.params;
+    const todo = todos.find((t) => t.id === parseInt(id));
+    todo.title = title;
+    res.json(todos);
+  });
+
 
 };
--- src/Labs/Lab5/WorkingWithArrays.tsx
+++ src/Labs/Lab5/WorkingWithArrays.tsx
@@ -3,7 +3,10 @@ import { FormControl } from "react-bootstrap";
 const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
 export default function WorkingWithArrays() {
   const API = `${REMOTE_SERVER}/lab5/todos`;
-  const [todo, setTodo] = useState({id: "1"});
+  const [todo, setTodo] = useState({id: "1", title: "NodeJS Assignment",
+    description: "Create a NodeJS server with ExpressJS",
+    due: "2021-09-09",
+    completed: false,});
   return (
     <div id="wd-working-with-arrays">
       <h3>Working with Arrays</h3>
@@ -31,6 +34,13 @@ export default function WorkingWithArrays() {
     <a id="wd-retrieve-completed-todos" className="btn btn-primary float-end" href={`${API}/${todo.id}/delete`}>
     Delete Todo with ID = {todo.id} </a>
     <FormControl defaultValue={todo.id} className="w-50" onChange={(e) => setTodo({ ...todo, id: e.target.value })}/><hr/>
-    
+    <h3>Updating an Item in an Array</h3>
+      <a href={`${API}/${todo.id}/title/${todo.title}`} className="btn btn-primary float-end">
+        Update Todo</a>
+      <FormControl defaultValue={todo.id} className="w-25 float-start me-2"
+        onChange={(e) => setTodo({ ...todo, id: e.target.value })}/>
+      <FormControl defaultValue={todo.title} className="w-50 float-start"
+             onChange={(e) => setTodo({ ...todo, title: e.target.value }) }/>
+      <br /><br /><hr />
     </div>
 );}
